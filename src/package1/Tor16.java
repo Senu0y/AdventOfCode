41edd4e1dacf21740b457f6ca48bb3591b5ee449
youne <youne@192.168.1.136>
2020-12-16 11:57:03 +0100

    16 schöner 16b weniger hässlich

--- src/package1/Tor16.java
+++ src/package1/Tor16.java
@@ -2,7 +2,6 @@ package package1;
 
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class Tor16 {
@@ -12,9 +11,8 @@ public class Tor16 {
 		File file = new File("tor16input");
 		Scanner scani = new Scanner(file);
 		String[] input = scani.useDelimiter("\\A").next().split("\\n");
-
 		String[] validnumbers = new String[20];
-
+		scani.close();
 		for (int i = 0; i < validnumbers.length; i++) {
 			validnumbers[i] = input[i].strip();
 		}
@@ -29,34 +27,27 @@ public class Tor16 {
 		for (int i = 0; i < tickets.length; i++) {
 			tickets[i] = input[i + 25];
 		}
-
 		String[][] ticketsAll = new String[236][30];
 		for (int i = 0; i < tickets.length; i++) {
 			ticketsAll[i] = tickets[i].split(",");
 		}
 
 		int sum = 0;
-		int[] canFit;
 		for (int i = 0; i < ticketsAll.length; i++) {
-			canFit = new int[20];
 			for (int j = 0; j < ticketsAll[i].length; j++) {
 				boolean isRange = false;
 				int number = Integer.parseInt(ticketsAll[i][j].strip());
 				for (int j2 = 0; j2 < validnumbersAll.length; j2++) {
 					if (isInRange(validnumbersAll[j2], number)) {
-						canFit[j] += 1;
 						isRange = true;
 					}
-
 				}
 				if (!isRange) {
 					sum += number;
 				}
-
 			}
 		}
 		System.out.println(sum);// 26869
-
 	}
 
 	static boolean isInRange(String[] s, int number) {
--- src/package1/Tor16b.java
+++ src/package1/Tor16b.java
@@ -9,14 +9,12 @@ import java.util.Scanner;
 
 public class Tor16b {
 	public static void main(String[] args) throws FileNotFoundException {
-	
+
 		File file = new File("tor16input");
 		Scanner scani = new Scanner(file);
 		String[] input = scani.useDelimiter("\\A").next().split("\\n");
-		// System.out.println(Arrays.toString(input));
-
 		String[] validnumbers = new String[20];
-
+		scani.close();
 		for (int i = 0; i < validnumbers.length; i++) {
 			validnumbers[i] = input[i].strip();
 		}
@@ -32,19 +30,12 @@ public class Tor16b {
 			tickets[i] = input[i + 25];
 		}
 
-		// System.out.println(Arrays.toString(tickets));
 		String[][] ticketsAll = new String[236][30];
 		for (int i = 0; i < tickets.length; i++) {
 			ticketsAll[i] = tickets[i].split(",");
 		}
-		// System.out.println((ticketsAll[235][0]));
-		// System.out.println(Arrays.deepToString((ticketsAll)));
-		// System.out.println(Arrays.toString(validnumbers));
-		// System.out.println(Arrays.deepToString((validnumbersAll)));
-		int sum = 0;
-		int[][] canFit = new int[ticketsAll.length][ticketsAll[0].length];
-		int[] howoften = new int[validnumbersAll.length];
-		boolean[] isnot= new boolean[20];
+
+		boolean[] isnot = new boolean[20];
 		ArrayList<HashSet<Integer>> jIsNot = new ArrayList<HashSet<Integer>>();
 		ArrayList<String[]> validtickets = new ArrayList<String[]>();
 		for (int i = 0; i < 21; i++) {
@@ -54,38 +45,28 @@ public class Tor16b {
 
 			for (int j = 0; j < ticketsAll[0].length; j++) {
 				boolean isRange = false;
-				// System.out.println(ticketsAll[i][j]);
 				int number = Integer.parseInt(ticketsAll[i][j].strip());
-				// System.out.println(j);
 				for (int j2 = 0; j2 < validnumbersAll.length; j2++) {
 					if (isInRange(validnumbersAll[j2], number)) {
 						if (j == 0) {
-							// howoften[j2]++;
-							// System.out.println(
-							// "zahl " + number + " regel " + j2 + " " + isInRange(validnumbersAll[j2],
-							// number));
 						}
 						isRange = true;
 					}
-
 				}
 				if (!isRange) {
 					// ticketsAll[i]=null;
-					sum += number;
+
 				}
 				if (isRange) {
 
 					for (int j2 = 0; j2 < validnumbersAll.length; j2++) {
 						if (!isInRange(validnumbersAll[j2], number)) {
 							if (j == 0) {
-								// howoften[j2]++;
 								System.out.println("zahl " + number + " regel " + j2 + " "
 										+ isInRange(validnumbersAll[j2], number));
-								isnot[j2]=true;
+								isnot[j2] = true;
 							}
 							jIsNot.get(j).add(j2);
-							
-
 						}
 
 					}
@@ -93,22 +74,22 @@ public class Tor16b {
 			}
 
 		}
-		
+
 		System.out.println(jIsNot.toString());
 		for (int i = 0; i < jIsNot.size(); i++) {
-			if(jIsNot.get(i).contains(0)) {
+			if (jIsNot.get(i).contains(0)) {
 				System.out.println(i);
 			}
-			
+
 		}
-		//System.out.println(Arrays.toString(isnot));
+		// System.out.println(Arrays.toString(isnot));
 		// System.out.println(ticketsAll.length);
 		// System.out.println(validtickets.size());
 		// System.out.println(validtickets.toString());
 		// System.out.println(Arrays.toString(howoften));
 		// System.out.println(Arrays.deepToString(canFit));
 		// System.out.println(sum);// 26869
-		System.out.println((long)73*137*151*67*79*107);
+		System.out.println((long) 73 * 137 * 151 * 67 * 79 * 107);
 	}
 
 	static boolean isInRange(String[] s, int number) {
@@ -119,7 +100,6 @@ public class Tor16b {
 			return true;
 		if (number >= range2[0] && number <= range2[1])
 			return true;
-	//	System.out.println(number);
 		return false;
 	}
 }
